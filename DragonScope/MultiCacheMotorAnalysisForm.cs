cbc995de432774272983cf636abb9444163764b2
notcharlie2026 <118229690+notcharlie2026@users.noreply.github.com>
2026-04-19 14:47:45 -0400

    Move all GUI declarations of MultiCacheMotorAnalysisForm to a Designer file

--- DragonScope/MultiCacheMotorAnalysisForm.cs
+++ DragonScope/MultiCacheMotorAnalysisForm.cs
@@ -11,120 +11,7 @@ namespace DragonScope
         public MultiCacheMotorAnalysisForm(DataCacheManager cacheManager)
         {
             _cacheManager = cacheManager ?? throw new ArgumentNullException(nameof(cacheManager));
-            InitializeComponent();
             LoadCacheList();
-        }
-
-        private void InitializeComponent()
-        {
-            topPanel = new Panel();
-            lblSeries = new Label();
-            btnAnalyze = new Button();
-            btnExport = new Button();
-            lblColors = new Label();
-            colorPanel = new FlowLayoutPanel();
-            lblCaches = new Label();
-            cacheListBox = new CheckedListBox();
-            seriesCombo = new ComboBox();
-            formsPlot = new FormsPlot();
-            SuspendLayout();
-            // 
-            // topPanel
-            // 
-            topPanel.Location = new Point(12, 168);
-            topPanel.Name = "topPanel";
-            topPanel.Size = new Size(200, 123);
-            topPanel.TabIndex = 0;
-            // 
-            // lblSeries
-            // 
-            lblSeries.Location = new Point(156, 125);
-            lblSeries.Name = "lblSeries";
-            lblSeries.Size = new Size(100, 23);
-            lblSeries.TabIndex = 2;
-            // 
-            // btnAnalyze
-            // 
-            btnAnalyze.Location = new Point(313, 105);
-            btnAnalyze.Name = "btnAnalyze";
-            btnAnalyze.Size = new Size(75, 23);
-            btnAnalyze.TabIndex = 4;
-            btnAnalyze.Text = "Analyze";
-            btnAnalyze.Click += BtnAnalyze_Click;
-            // 
-            // btnExport
-            // 
-            btnExport.Location = new Point(394, 105);
-            btnExport.Name = "btnExport";
-            btnExport.Size = new Size(75, 23);
-            btnExport.TabIndex = 5;
-            btnExport.Text = "Export";
-            btnExport.Click += BtnExport_Click;
-            // 
-            // lblColors
-            // 
-            lblColors.Location = new Point(185, 102);
-            lblColors.Name = "lblColors";
-            lblColors.Size = new Size(100, 23);
-            lblColors.TabIndex = 6;
-            // 
-            // colorPanel
-            // 
-            colorPanel.Location = new Point(227, 168);
-            colorPanel.Name = "colorPanel";
-            colorPanel.Size = new Size(200, 123);
-            colorPanel.TabIndex = 7;
-            // 
-            // lblCaches
-            // 
-            lblCaches.Location = new Point(207, 102);
-            lblCaches.Name = "lblCaches";
-            lblCaches.Size = new Size(100, 23);
-            lblCaches.TabIndex = 0;
-            // 
-            // cacheListBox
-            // 
-            cacheListBox.Location = new Point(317, 41);
-            cacheListBox.Name = "cacheListBox";
-            cacheListBox.Size = new Size(134, 58);
-            cacheListBox.TabIndex = 1;
-            cacheListBox.ItemCheck += CacheListBox_ItemCheck;
-            // 
-            // seriesCombo
-            // 
-            seriesCombo.Location = new Point(317, 12);
-            seriesCombo.Name = "seriesCombo";
-            seriesCombo.Size = new Size(121, 23);
-            seriesCombo.TabIndex = 3;
-            seriesCombo.SelectedIndexChanged += SeriesCombo_SelectedIndexChanged;
-            // 
-            // formsPlot
-            // 
-            formsPlot.DisplayScale = 1F;
-            formsPlot.Location = new Point(0, 12);
-            formsPlot.Name = "formsPlot";
-            formsPlot.Size = new Size(150, 150);
-            formsPlot.TabIndex = 1;
-            // 
-            // MultiCacheMotorAnalysisForm
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(512, 303);
-            Controls.Add(lblColors);
-            Controls.Add(btnExport);
-            Controls.Add(colorPanel);
-            Controls.Add(btnAnalyze);
-            Controls.Add(lblSeries);
-            Controls.Add(lblCaches);
-            Controls.Add(topPanel);
-            Controls.Add(cacheListBox);
-            Controls.Add(seriesCombo);
-            Controls.Add(formsPlot);
-            Name = "MultiCacheMotorAnalysisForm";
-            StartPosition = FormStartPosition.CenterParent;
-            Text = "Multi-Cache Motor Analysis";
-            ResumeLayout(false);
         }
 
         private void LoadCacheList()
@@ -531,18 +418,5 @@ namespace DragonScope
                 System.Drawing.Color.Magenta
             };
         }
-
-        private System.ComponentModel.IContainer components;
-
-        private Panel topPanel;
-        private System.Windows.Forms.Label lblCaches;
-        private CheckedListBox cacheListBox;
-        private System.Windows.Forms.Label lblSeries;
-        private ComboBox seriesCombo;
-        private Button btnAnalyze;
-        private Button btnExport;
-        private System.Windows.Forms.Label lblColors;
-        private FlowLayoutPanel colorPanel;
-        private FormsPlot formsPlot;
     }
 }
